4f5b5040bbb7910d60c92ac6163add4afbe19dda
Gallelee <thimotus@gmail.com>
2022-09-28 23:11:28 -0400

    update link for pages

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const assignments = [
         type: "HW",
         dueDate: "9/29",
         description: "",
-        link: "CartoonAssignment/index.html"
+        link: "3610PortfolioSite/CartoonAssignment/index.html"
     }
 ]
 
